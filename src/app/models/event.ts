1dfa1c506472cb4e9a2344b08cb781cb26d22d5b
Sotheanith Sok <thetheanith@gmail.com>
2019-04-20 16:57:25 -0700

    Add skeleton codes

---
+++ src/app/models/event.ts
@@ -0,0 +1,13 @@
+export class Event {
+    id: string
+    class: string
+    instructor: string
+    duration: number
+    location: string
+    topic: string[]
+    date: number
+    description: string
+    currentCapacity: number
+    maxCapacity: number
+    organizer: string
+}
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { EventDetailComponent } from '../../components/event-detail/event-detail.component';
+import { NavigationBarComponent } from '../../components/navigation-bar/navigation-bar.component';
+import { FilterBarComponent } from '../../components/filter-bar/filter-bar.component';
+import { EventsListComponent } from '../../components/events-list/events-list.component';
+import { EventFormComponent } from '../../components/event-form/event-form.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, EventDetailComponent, NavigationBarComponent, FilterBarComponent, EventsListComponent, EventFormComponent],
   imports: [
     CommonModule,
     MainRoutingModule
