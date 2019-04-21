509a6d014b7332face9c705bbc0b3f1861c1fa45
Phoenix-007 <pravinpatare007@gmail.com>
2019-04-20 23:11:17 -0700

    Day 1 upload

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -8,9 +8,10 @@ import { NavigationBarComponent } from '../../components/navigation-bar/navigati
 import { FilterBarComponent } from '../../components/filter-bar/filter-bar.component';
 import { EventsListComponent } from '../../components/events-list/events-list.component';
 import { EventFormComponent } from '../../components/event-form/event-form.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @NgModule({
-  declarations: [MainComponent, EventDetailComponent, NavigationBarComponent, FilterBarComponent, EventsListComponent, EventFormComponent],
+  declarations: [MainComponent, EventDetailComponent, NavigationBarComponent, FilterBarComponent, EventsListComponent, EventFormComponent, FooterComponent],
   imports: [
     CommonModule,
     MainRoutingModule
